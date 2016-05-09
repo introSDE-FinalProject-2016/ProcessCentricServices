f58d2052faa4a96b6cd2a12f816c83746aff5fa9
yuly-sanchez <yuly.sanchez29@gmail.com>
2016-05-09 12:31:42 +0200

    update services's heroku url

--- src/introsde/finalproject/rest/processcentricservices/util/UrlInfo.java
+++ src/introsde/finalproject/rest/processcentricservices/util/UrlInfo.java
@@ -4,9 +4,15 @@ public class UrlInfo {
 	
 	public UrlInfo() {}
 	
-	static final String businessLogicUrl = "http://127.0.1.1:5700/sdelab/businessLogic-service";
-	static final String storageUrl = "http://127.0.1.1:5701/sdelab/storage-service";
-	static final String adapterUrl = "http://127.0.1.1:5702/sdelab/adapter-service";
+	//static final String businessLogicUrl = "http://127.0.1.1:5700/sdelab/businessLogic-service";
+	static final String businessLogicUrl = "https://fierce-sea-36005.herokuapp.com/sdelab/businessLogic-service";
+	
+	//static final String storageUrl = "http://127.0.1.1:5701/sdelab/storage-service";
+	static final String storageUrl = "https://warm-hamlet-95336.herokuapp.com/sdelab/storage-service";
+	
+	//static final String adapterUrl = "http://127.0.1.1:5702/sdelab/adapter-service";
+	static final String adapterUrl = "https://desolate-scrubland-21919.herokuapp.com/sdelab/adapter-service";
+	
 	static final String processCentricUrl = "http://127.0.1.1:5703/sdelab/processCentric-service";
 	
 	/**
