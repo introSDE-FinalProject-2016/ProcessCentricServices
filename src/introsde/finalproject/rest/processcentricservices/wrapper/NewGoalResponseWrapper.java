2a425f6248edc3e257857b1952ec640900b06725
yuly-sanchez <yuly.sanchez29@gmail.com>
2016-06-27 23:24:26 +0200

    add newGoalResponseWrapper class

---
+++ src/introsde/finalproject/rest/processcentricservices/wrapper/NewGoalResponseWrapper.java
@@ -0,0 +1,26 @@
+package introsde.finalproject.rest.processcentricservices.wrapper;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@XmlRootElement(name="Response")
+public class NewGoalResponseWrapper {
+	
+	@XmlElement(name="goals")
+	@JsonProperty("goals")
+	public GoalList goalList = new GoalList();
+	
+	@XmlElement(name="url")
+	@JsonProperty("url")
+	public String url = new String();
+	
+	public void setGoal(GoalList goalList) {
+		this.goalList = goalList;
+	}
+	
+	public void setURL(String url){
+		this.url = url;
+	}
+}
